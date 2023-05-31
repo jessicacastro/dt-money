3d2172a26db2f8b89f363a993e45f4649e5c32c6
Jessica Castro <jessica.castros@outlook.com>
2023-05-31 20:38:54 -0300

    chore: initialize dt-money app with header and pages

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+});
